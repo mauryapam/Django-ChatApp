201258aa2060b822b93bbb869aad22e37797fbf2
unknown <mauryapam@gmail.com>
2021-08-09 14:45:38 +0530

    username added

--- Chat/consumers.py
+++ Chat/consumers.py
@@ -19,12 +19,13 @@
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.contrib.auth.models import User
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         
         self.room_group_name = 'test' 
-
+        #self.username = User.username
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
@@ -38,27 +39,32 @@ class ChatConsumer(WebsocketConsumer):
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
+            
         )
 
     # Receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        username= text_data_json['username']
 
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
+                'username':username,
                 'type': 'chat_message',
                 'message': message
+
             }
         )
 
     # Receive message from room group
     def chat_message(self, event):
         message = event['message']
-
+        username= event['username']
         # Send message to WebSocket
         self.send(text_data=json.dumps({
+            'username': username,
             'message': message
         }))
